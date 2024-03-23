766665b978d474e12c27ff40dd23d52a8e00e0d0
Harsh Naik <109041414+kianhsrah@users.noreply.github.com>
2024-03-23 10:41:38 -0400

    Create Fizzbuzz.java

---
+++ rosettaCode/fizzbuzz/Fizzbuzz.java
@@ -0,0 +1,13 @@
+package rosettaCode.fizzbuzz;
+
+public class Fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            String output = "";
+            if (i % 3 == 0) output += "Fizz";
+            if (i % 5 == 0) output += "Buzz";
+            System.out.println(output.isEmpty() ? i : output); // print i if output is empty
+        }
+    }
+
+}
